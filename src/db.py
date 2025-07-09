d9a7812569ef64d7a16609c7f7a42f9d879e389c
Miguel <michail.pliuscik@gmail.com>
2025-07-09 16:13:37 +0100

    fixed db file to load railway variables

--- src/db.py
+++ src/db.py
@@ -1,9 +1,11 @@
 import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from dotenv import load_dotenv
 
-load_dotenv()
+# Only load .env locally
+if os.getenv("RAILWAY_ENVIRONMENT") is None:
+    from dotenv import load_dotenv
+    load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
